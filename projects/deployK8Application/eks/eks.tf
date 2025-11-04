5f76c86dbba60b5bdd19f1d489c6d1980f55ea9a
jmpires <47697862+jmpires@users.noreply.github.com>
2025-11-04 16:49:58 +0000

    updated eks and add versions.tf

--- projects/deployK8Application/eks/eks.tf
+++ projects/deployK8Application/eks/eks.tf
@@ -1,28 +1,26 @@
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
-
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "~> 19.0"
   cluster_name    = "my-eks-cluster"
-  cluster_version = "1.28" # 1.24 is deprecated — use 1.27+)
+  cluster_version = "1.24"
+
+  cluster_endpoint_public_access = true
 
   vpc_id     = module.my-vpc.vpc_id
   subnet_ids = module.my-vpc.private_subnets
 
-  # Public access config changed in v20+
-  cluster_endpoint_public_access  = true
-  cluster_endpoint_private_access = true
-
-  eks_managed_node_groups = {
-    dev = {
-      min_size       = 1
-      max_size       = 3
-      desired_size   = 2
-      instance_types = ["t3.small"] # t2.small not supported in all regions for EKS
-    }
-  }
-
   tags = {
     environment = "development"
     application = "nginx-app"
+  }
+
+  eks_managed_node_groups = {
+    dev = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      instance_types = ["t2.small"]
+    }
   }
 }
