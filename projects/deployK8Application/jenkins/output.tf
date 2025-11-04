5ff594063498135c4b0d4c1008edb5e2657f799d
jmpires <47697862+jmpires@users.noreply.github.com>
2025-11-04 20:44:54 +0000

    redo the jenkins directory

--- projects/deployK8Application/jenkins/output.tf
+++ projects/deployK8Application/jenkins/output.tf
@@ -3,6 +3,6 @@ output "ec2_public_ip" {
   value = aws_instance.my-server.public_ip
 }
 
-output "public_subnets" {
-  value = module.my-vpc.public_subnets
-}
+#output "public_subnets" {
+#  value = module.my-vpc.public_subnets
+#}
