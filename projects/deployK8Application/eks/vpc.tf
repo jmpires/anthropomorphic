51aa3b95a0777ba427bd365a22c23e571bb5de21
jmpires <47697862+jmpires@users.noreply.github.com>
2025-11-04 15:43:05 +0000

    updated file location

--- projects/deployK8Application/eks/vpc.tf
+++ projects/deployK8Application/eks/vpc.tf
@@ -19,11 +19,11 @@ module "my-vpc" {
 
   public_subnet_tags = {
     "kubernetes.io/cluster/my-eks-cluster" = "shared"
-    "kubernetes.io/role/elb"                  = 1
+    "kubernetes.io/role/elb"               = 1
   }
 
   private_subnet_tags = {
     "kubernetes.io/cluster/my-eks-cluster" = "shared"
-    "kubernetes.io/role/internal-elb"         = 1
+    "kubernetes.io/role/internal-elb"      = 1
   }
 }
