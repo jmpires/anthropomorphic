ff24d3d95693689d950325f4b1e89154d42d5ce9
jmpires <47697862+jmpires@users.noreply.github.com>
2025-11-05 16:07:03 +0000

    updated the script

--- projects/deployK8Application/jenkins/jenkins-script.sh
+++ projects/deployK8Application/jenkins/jenkins-script.sh
@@ -49,6 +49,14 @@ sudo yum -y install terraform
 
 # Install kubectl
 echo "=== Installing kubectl ==="
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x kubectl
-sudo mv kubectl /usr/local/bin/kubectl
+sudo curl -LO "https://dl.k8s.io/release/v1.28.0/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl
+
+# Install AWS CLI v2
+echo "=== Installing AWS CLI v2 ==="
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf awscliv2.zip aws/
+
