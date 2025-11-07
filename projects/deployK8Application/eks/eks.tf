69e69750a5c2f6134ba2eaf8a8a155b4e36d63f2
jmpires <47697862+jmpires@users.noreply.github.com>
2025-11-07 20:09:42 +0000

    update eks.tf

--- projects/deployK8Application/eks/eks.tf
+++ projects/deployK8Application/eks/eks.tf
@@ -1,37 +1,9 @@
 # eks.tf
 
-/*
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "~> 19.0"
-  cluster_name    = "jenkins-eks-cluster"
-  cluster_version = "1.28"
-
-  cluster_endpoint_public_access = true
-
-  vpc_id     = module.my-vpc.vpc_id
-  subnet_ids = module.my-vpc.private_subnets
-
-  tags = {
-    environment = "development"
-    application = "nginx-app"
-  }
-
-  eks_managed_node_groups = {
-    eks_node = {
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-
-      instance_types = ["t2.small"]
-    }
-  }
-}
-*/
-
-module "eks" {
-  source = "terraform-aws-modules/eks/aws"
-  version         = "~> 20.0"
+  #version         = "~> 20.0"
   cluster_name    = "jenkins-eks-cluster"
   cluster_version = "1.28"
 
