9ee740b1fd0634c4fc126b2436e49e7bef0a8e81
jmpires <47697862+jmpires@users.noreply.github.com>
2025-12-12 16:45:23 +0000

    updated

--- projects/deployK8Application/eks/eks.tf
+++ projects/deployK8Application/eks/eks.tf
@@ -15,12 +15,21 @@ module "eks" {
     application = "nginx-app"
   }
 
+  # -------------------------------
+  # Give Jenkins EC2 instance access
+  # -------------------------------
+  cluster_access_entries = {
+    jenkins_ec2_role = {
+      principal_arn     = "arn:aws:iam::682882937469:role/JenkinsEKSRole" # replace with your EC2 instance role
+      kubernetes_groups = ["system:masters"]
+    }
+  }
+
   eks_managed_node_groups = {
     eks_node = {
-      min_size     = 1
-      max_size     = 3
-      desired_size = 2
-
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
       instance_types = ["t3.small"]
       ami_type       = "AL2023_x86_64_STANDARD"
     }
