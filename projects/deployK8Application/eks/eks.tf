eb974a63c017f65e7353ee724deb753972dbbbbc
jmpires <47697862+jmpires@users.noreply.github.com>
2025-12-12 18:49:19 +0000

    update

--- projects/deployK8Application/eks/eks.tf
+++ projects/deployK8Application/eks/eks.tf
@@ -15,15 +15,6 @@ module "eks" {
     application = "nginx-app"
   }
 
-  # Grant Jenkins IAM Role full cluster admin access
-  map_roles = [
-    {
-      rolearn  = "arn:aws:iam::682882937469:role/JenkinsEKSRole"
-      username = "jenkins-admin"
-      groups   = ["system:masters"]
-    }
-  ]
-
   eks_managed_node_groups = {
     eks_node = {
       min_size       = 1
@@ -33,4 +24,41 @@ module "eks" {
       ami_type       = "AL2023_x86_64_STANDARD"
     }
   }
+}
+
+# -------------------------------------------------
+# Kubernetes Provider pointing to EKS Cluster
+# -------------------------------------------------
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+# -------------------------------------------------
+# Map Jenkins IAM Role to system:masters
+# -------------------------------------------------
+resource "kubernetes_config_map" "aws_auth" {
+  depends_on = [module.eks]
+
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = yamlencode([
+      {
+        rolearn  = "arn:aws:iam::682882937469:role/JenkinsEKSRole"
+        username = "jenkins-admin"
+        groups   = ["system:masters"]
+      }
+    ])
+  }
+
+  provider = kubernetes
 }
