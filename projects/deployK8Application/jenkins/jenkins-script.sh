ff255c91499c9dbee4fdcba36cd4ff73d1b9e066
jmpires <47697862+jmpires@users.noreply.github.com>
2025-12-12 16:11:00 +0000

    updated bootstrap script

--- projects/deployK8Application/jenkins/jenkins-script.sh
+++ projects/deployK8Application/jenkins/jenkins-script.sh
@@ -4,10 +4,12 @@
 # OS supported: MacOS, Linux
 # Author: Jorge Manuel Pires
 # Contributors: Jorge Manuel Pires
-# Initial Version.Last Updated(updates)	:v20251106.v20251212(5)
+# Initial Version.Last Updated(updates)	:v20251106.v20251212(6)
 
 # For debugging purposes
 # set -x
+
+set -e
 
 # -------------------------------------------------
 # Update package repository and installed packages
@@ -15,44 +17,45 @@ sudo yum update -y
 
 # -------------------------------------------------
 # Install Java 17 (required by modern Jenkins)
-# Jenkins >= 2.357 requires Java 17 or 21
-# Using Amazon Corretto: official, LTS, and fully supported
-# -------------------------------------------------
 sudo yum install -y java-17-amazon-corretto-headless
+java -version
 
 # -------------------------------------------------
 # Install Jenkins from official stable repository
-# -------------------------------------------------
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum install -y jenkins
-
-# Enable and start Jenkins service
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 
 # -------------------------------------------------
-# Install Git (required for SCM integration)
-# -------------------------------------------------
+# Install Git
 sudo yum install -y git
 
 # -------------------------------------------------
-# Install Terraform (for infrastructure orchestration)
-# -------------------------------------------------
+# Install Terraform
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum install -y terraform
 
 # -------------------------------------------------
-# Install kubectl (for EKS interaction)
-# Uses latest stable version from official Kubernetes release channel
-# -------------------------------------------------
-sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+# Install latest kubectl
+LATEST_KUBECTL=$(curl -L -s https://dl.k8s.io/release/stable.txt)
+curl -LO "https://dl.k8s.io/release/${LATEST_KUBECTL}/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
+# -------------------------------------------------
+# Install AWS CLI v2 (required for EKS authentication)
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install --update
+rm -rf awscliv2.zip aws
+
+# -------------------------------------------------
 # Optional: verify all tools
-# java -version
-# jenkins --version
-# git --version
-# terraform -version
-# kubectl version --client
+aws --version
+kubectl version --client
+terraform -version
+git --version
+java -version
+jenkins --version
